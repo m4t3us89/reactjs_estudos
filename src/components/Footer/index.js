446856e1f503cdc16b7b2ec707566cbb2f1f9252
Allisson Mateus <allisson.mateus@crptecnologia.com.br>
2019-07-12 15:16:18 -0300

    mudança rodapé

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,4 +1,4 @@
 import React from 'react'
 import './styles.css'
 
-export default () => <footer id="main-footer">JSHunt</footer>
+export default () => <footer id="main-footer">Todos os direitos reservados {new Date().getFullYear()}</footer>
